93f53880f5cccff0b3d974857a92d768d7e57b8a
Juliana Lemos <lemos.julianajesus@gmail.com>
2025-03-21 13:29:53 -0500

    possible next config error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  trailingSlash: false, // Ensure URLs are handled correctly
+  async rewrites() {
+    return [
+      {
+        source: "/(.*)", 
+        destination: "/", 
+      },
+    ];
+  },
 };
 
 export default nextConfig;
